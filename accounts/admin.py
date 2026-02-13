71b4a9bbd4dfaeb150de9372a82012ec5de6eaf3
Jack Contro <jack.contro@gmail.com>
2026-02-13 12:49:37 +0000

    admin side sort of working

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, UserProfile
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_email_verified', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_email_verified')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Verification', {'fields': ('is_email_verified', 'verification_token', 'verification_token_created_at')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'updated_at')
+    search_fields = ('user__email', 'user__username')
